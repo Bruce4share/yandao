bb716bd101ab2f007ed48b565bd31f67d7abbede
=nic <nicminika@gmail.com>
2014-10-29 14:44:40 +0800

    10-29-scrolljs

--- js/common.js
+++ js/common.js
@@ -1,47 +1,46 @@
 $(function(){
 	$('.banner-title').addClass('anima-fade');
-	// var iNum = 0;
-	// 	document.body.onmousewheel = scrollfunc;
- //        document.body.addEventListener("DOMMouseScroll", scrollfunc,false);
- //        // function(event){
- //        //         event = event || window.event;
- //        //         iNum = event.wheelDelta;
- //        //         console.log(iNum);
- //        // }
- //        // function(event){
- //        // 		 iNum = event.detail*-40;
- //        // 		 // return iNum;
- //        //         console.log(iNum);
- //        // }
- //        var intval;
- //        function scrollfunc(event){
- //        	var iStrar = 0;
- //        	var iScrolltop = $(window).scrollTop();
- //        	var iTime = 700;
- //        	var iSect = iScrolltop/(iTime/20);
+	$('.nav-link').each(function(){
+		$(this).hover(function(){
+			$(this).siblings('dl').slideDown();
+		},function(){
+			$(this).siblings('dl').slideUp();
+		});
+	});
+	var iNum = 0;
+	var cons = true;
+		document.body.onmousewheel = scrollfunc;
+        document.body.addEventListener("DOMMouseScroll", scrollfunc,false);
+        var intval;
+        function scrollfunc(event){
+        	var iStrar = 0;
+        	var iScrolh = $(window).height();
+        	var iTime = 400;
+        	var iSect = iScrolh/(iTime/20);
 
+        	event = event || window.event;
 
- //        	event = event || window.event;
- //        	if(event.wheelDelta){
- //        		iNum = event.wheelDelta;
- //        	}else{
- //        		iNum = event.detail*-40;
- //        	}
- //        	if(iNum==-120){
- //        		intval = setInterval(function(){
- //        			iStrar+=iSect;
- //        			intvanfn(iStrar);
- //        		},20);
- //        	}else if(iNum==120){
- //        		console.log('shang');
- //        	}
- //        }
- //        function intvanfn(iStrar){
- //        	if($(window).scrollTop()<$(window).height()){
- //        		$(window).scrollTop(iStrar);
- //        	}else{
- //        		clearInterval(intval);
- //        	}
- //        }
- //        	// console.log(iNum);
+        	if(cons){
+        		if(event.wheelDelta){
+	        		iNum = event.wheelDelta;
+	        	}else{
+	        		iNum = event.detail*-40;
+	        	}
+	        	intval = setInterval(function(){
+	        		if(iNum == -120){
+	        			iStrar+=iSect;
+	        			intvanfn(iStrar);
+	        			cons = false;
+	        		}
+	        		event.preventDefault();
+	        	},20);
+        	}
+        }
+        function intvanfn(iStrar){
+        	if($(window).scrollTop()<$(window).height()){
+        		$(window).scrollTop(iStrar);
+        	}else{
+        		clearInterval(intval);
+        	}
+        }
 });
